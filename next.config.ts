6f987ab4b904707ef25f5df0a83e69192ad4d6a3
madzwritescode <156851422+madzwritescode@users.noreply.github.com>
2026-03-08 21:31:44 +0000

    summit timeline calculator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/summit-timeline-calculator",
+        destination: "/summit-timeline-calculator/index.html",
+      },
+      {
+        source: "/summit-timeline-calculator/",
+        destination: "/summit-timeline-calculator/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
